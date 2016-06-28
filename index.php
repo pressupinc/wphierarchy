9cf3ee401f4f36233dcc85052acb25ef85c5e77a
Rami Abraham <r@radesign.ws>
2016-06-28 02:07:48 -0400

    Add contributors link to credits on the site.

--- index.php
+++ index.php
@@ -48,7 +48,7 @@ require "includes/rss.php";
 					<span><?= _( 'Design'); ?></span> <a class="ref" href="http://marktimemedia.com/">Michelle Schulp</a>,
 					<span><?= _( 'Code:'); ?></span> <a class="ref" href="https://ramiabraham.com">Rami Abraham</a>,
 					<span><?= _( 'Icons:'); ?></span> <a class="ref" href="https://developer.wordpress.org/resource/dashicons/#admin-collapse">Dashicons</a> by <a class="ref" href="https://choycedesign.com/">Mel Choyce</a>,
-					<span><?= _( 'Japanese translation'); ?>:</span> <a class="ref" href="http://unofficialtokyo.com/">Akira Tachibana</a>
+					<span><?= _( 'Japanese translation'); ?>:</span> <a class="ref" href="http://unofficialtokyo.com/">Akira Tachibana</a>, &hearts; and <a href="https://github.com/ramiabraham/wphierarchy/graphs/contributors">contributors</a>.
 				</p>
 				</div>
 			</div>
